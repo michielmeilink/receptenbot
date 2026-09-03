47e863ed690101c2b0981402a44deff92a0172c6
Michiel Meilink <10865148+michielmeilink@users.noreply.github.com>
2026-09-03 15:45:35 +0200

    Koppel Google Docs recepten

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Plak hier na het publiceren de /exec-URL van de Google Apps Script-webapp.
-window.RECEPTEN_API_URL = "";
+window.RECEPTEN_API_URL = "https://script.google.com/macros/s/AKfycbwoqqQ_p13h8iXeGYiqBbYoDsfuO-A01t8_XR0pRVjoBy-MIwRcmkEFIB0i-7T-koa-/exec";
